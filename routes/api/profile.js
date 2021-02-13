534a99e14970b784f40ea688aca33f16f790c804
Nitish Kumar <krnitish@live.in>
2021-02-13 18:19:15 +0530

    Update Profile Config

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
+const { check, validationResult } = require("express-validator/check")
 
 // @route  GET api/profile/me
 // @desc   Get current userprofile
@@ -19,5 +20,19 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error')
     }
 });
+
+// @route  POST api/profile
+// @desc   Create or update User profile
+// @access Private
+
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skill is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: error.array() })
+    }
+})
 
 module.exports = router;
